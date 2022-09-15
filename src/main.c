d9ee9046dba1d4b5f43b49a692b6576d07fb392a
jham <irang4605@naver.com>
2022-09-16 00:33:51 +0900

    up

--- src/main.c
+++ src/main.c
@@ -9,8 +9,35 @@
 /*   Updated: 2022/09/07 16:33:39 by hyunhole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "../include/minishell.h"
 
-int	main(void)
+void main_init(int argc, char *argv[])
 {
+	struct termios term;
+
+	/*/
+		if (argc != 1)
+			exit_with_err("argument input error", NULL, 126);
+	/*/
+	tcgetattr(STDIN_FILENO, &term);
+	term.c_cflag &= ~(ECHOCTL);
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+	set_signal(SHE, SHE);
+	g_exit_code = 0;
+	(void)argc;
+	(void)argv;
+}
+
+int main(int argc, char *argv[], char *envp[])
+{
+	char *line;
+	t_cmd *cmd;
+	t_env env_head;
+	struct termios term;
+
+	tcgetattr(STDIN_FILENO, &term);
+	main_init(argc, argv);
+	init_env_list(&env_head, envp);
+
 	return (0);
 }
---
+++ utiles/signal.c
@@ -0,0 +1,9 @@
+#include "utiles.h"
+
+void set_signal(int sig_int, int sig_quit)
+{
+  if (sig_int == IGN) //왜안먹히지
+
+    if (sig_int == SHE)
+      signal(SIGINT, signal_handler);
+}
